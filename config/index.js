28cc004ce86ac852fe620af461214a8f6d7ec443
HafsaI <hafsairfan80@gmail.com>
2023-06-25 19:23:55 +0500

    Add input file reading

---
+++ config/index.js
@@ -0,0 +1,28 @@
+const config = {
+    
+    commands: Object.freeze({
+        Allocate: 'ALLOCATE',
+        Sip:      'SIP',
+        Change:   'CHANGE',
+        Balance:  'BALANCE',
+        Rebalance:'REBALANCE'
+    }),
+    
+    months: Object.freeze({
+        JANUARY: 'JANUARY',
+        FEBRUARY:  'FEBRUARY',
+        MARCH: 'MARCH',
+        APRIL:  'APRIL',
+        MAY: 'MAY',
+        JUNE: 'JUNE',
+        JULY: 'JULY',
+        AUGUST: 'AUGUST',
+        SEPTEMBER: 'SEPTEMBER',
+        OCTOBER: 'OCTOBER',
+        NOVEMBER: 'NOVEMBER',
+        DECEMBER: 'DECEMBER'
+    }),
+}
+
+module.exports = config;
+
---
+++ geektrust.js
@@ -0,0 +1,21 @@
+// const InputHandler = require('./utils/InputHandler')
+const fs = require('fs')
+
+
+const filePath = process.argv[2]
+if (!filePath) {
+    console.log(
+      'No input file path provided. Usage: node index.js <input-file-path>'
+    );
+}
+else{
+    try {
+        inputData = fs.readFileSync(filePath, "utf8",{flag:'r'})
+        if (inputData){
+            inputData = inputData.toString().trim().split('\n');
+            // obj = new InputHandler(inputData)
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
